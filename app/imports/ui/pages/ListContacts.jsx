963befec828796a08fcd895c2a7f9990668a585e
jay <jsuh4444@hawaii.edu>
2024-03-31 23:41:20 -1000

    Update ListContacts.jsx

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -4,7 +4,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import Contact from '../components/Contact';
-import { Contacts } from '../../api/contact/Contacts';
+import { Contacts } from '../../api/Contacts/Contacts';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const ListContacts = () => {
@@ -16,11 +16,7 @@ const ListContacts = () => {
     const subscription = Meteor.subscribe(Contacts.userPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
-<<<<<<< HEAD
     // Get the Stuff documents
-=======
-    // Get the Contact documents
->>>>>>> contacts-collection-2
     const contactItems = Contacts.collection.find({}).fetch();
     return {
       contacts: contactItems,
